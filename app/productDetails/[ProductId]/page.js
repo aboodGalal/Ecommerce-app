09da2570aab647380942bf78c52d45c1be80ab86
Your Name <aboodelking321@gmail.com>
2024-06-16 00:22:58 +0200

    Converting the Breadcrumb to dunamic component by usePath from next and split it for outputting

--- app/productDetails/[ProductId]/page.js
+++ app/productDetails/[ProductId]/page.js
@@ -2,16 +2,18 @@
 import Breadcrumb from '@/app/_components/Breadcrumb'
 import ProductList from '@/app/_components/ProductList'
 import ProdcutsApies from '@/app/_utils/ProdcutsApies'
+import { usePathname } from 'next/navigation'
 import React, { useEffect, useState } from 'react'
 
 function ProductDetails({ params }) {
   const id = params?.productID
   const [productData, setProductData] = useState()
   const [productList, setProductList] = useState([])
+  const path = usePathname().split("/")
 
   useEffect(() => {
     getProductId_()
-    // console.log(id)
+    console.log(path)
   }, [])
 
   const getProductId_ = () => {
@@ -31,7 +33,7 @@ function ProductDetails({ params }) {
   }
   return (
     <div className={`flex flex-col gap-3 px-10 md:px-20 w-full`}>
-      <Breadcrumb />
+      <Breadcrumb path={path}/>
       <div className=' inline-flex flex-col md:flex-row gap-5 md:gap-20'>
         <img className={`min-h-[200px] max-h-[250px] max-w-[450px] object-cover rounded-lg`}
           src={productData?.attributes?.image?.data?.attributes?.url} alt="image-card" property='image-card' />
